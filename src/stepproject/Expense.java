2891fcb041eb23f6d32bba32fe064a79781cb737
farhan <mohammedfarhanyusuf@gmail.com>
2026-02-12 10:09:09 +0530

    feat(init): initial project setup

--- src/stepproject/Expense.java
+++ src/stepproject/Expense.java
@@ -1,7 +1,20 @@
 package stepproject;
-
+import java.util.Scanner;
 public class Expense {
     public static void main(String[] args){
-        System.out.println("expense");
+       income s = new income();
+       int e = s.ravi();
+       System.out.println(e);
+    }
+}
+class income{
+    int ravi() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your salary : ");
+        int salary = sc.nextInt();
+        System.out.println("Enter the bonus : ");
+        int bonus = sc.nextInt();
+        int s = bonus + salary ;
+        return s;
     }
 }
